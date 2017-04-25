b1cb12922b189ae18631d3f94c2ee1d76a22623c
CPritchard <pritchardc2015@gmail.com>
2017-04-25 17:48:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):  #triggers on an update
     #need to add a clear display
     if msg.topic.find('/374586/') != -1:  #checks if the topic is the needed one
-        #print(msg.topic + " " + float(msg.payload))
+        value = str(msg.payload).strip("b")
+        value = value.strip("'")
+        print(msg.topic + " " + value)
         if msg.topic.find('sensor1') != -1:
             lcd.set_cursor_position(0,0)
-            lcd.write('pm10 ' + float(msg.payload))
+            lcd.write('pm10 ' + value)
             
         if msg.topic.find('sensor1') != -1:
             lcd.set_cursor_position(11,0)
-            lcd.write('pm25 ' + float(msg.payload))
+            lcd.write('pm25 ' + value)
             
         if msg.topic.find('sensor1') != -1:
             lcd.set_cursor_position(0,1)
-            lcd.write('temp ' + float(msg.payload))
+            lcd.write('temp ' + value)
             
         if msg.topic.find('sensor1') != -1:
             lcd.set_cursor_position(0,2)
-            lcd.write('hum ' + float(msg.payload))
+            lcd.write('hum ' + value)
     
 client = mqtt.Client()
 client.connect("mqtt.unixweb.de",1883,60)  #connects to the broker
