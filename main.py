c9c2b859c036fa47de5eeca2f5fb432f9e0e86df
CPritchard <pritchardc2015@gmail.com>
2017-04-26 08:37:09 +0100

    Add files via upload
    Changed sensor names and also pm25's value is written under it, should add real time under that again.

--- main.py
+++ main.py
@@ -14,25 +14,31 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe('#')
 
 def on_message(client, userdata, msg):  #triggers on an update
-    #need to add a clear display
     if msg.topic.find('/374586/') != -1:  #checks if the topic is the needed one
-        value = str(msg.payload).strip("b")
-        value = value.strip("'")
-        print(msg.topic + " " + value)
         if msg.topic.find('sensor1') != -1:
             lcd.set_cursor_position(0,0)
+            value = str(msg.payload).strip("b")  #strips unneeded parts from the values
+            value = value.strip("'")
             lcd.write('pm10 ' + value)
             
-        if msg.topic.find('sensor1') != -1:
+        if msg.topic.find('sensor2') != -1:
             lcd.set_cursor_position(11,0)
-            lcd.write('pm25 ' + value)
+            value = str(msg.payload).strip("b")
+            value = value.strip("'")
+            lcd.write('pm25')
+            lcd.set_cursor_position(11, 1)  #writes the numerical value under the word
+            lcd.write(value)
             
-        if msg.topic.find('sensor1') != -1:
+        if msg.topic.find('sensor3') != -1:
             lcd.set_cursor_position(0,1)
+            value = str(msg.payload).strip("b")
+            value = value.strip("'")
             lcd.write('temp ' + value)
             
-        if msg.topic.find('sensor1') != -1:
+        if msg.topic.find('sensor4') != -1:
             lcd.set_cursor_position(0,2)
+            value = str(msg.payload).strip("b")
+            value = value.strip("'")
             lcd.write('hum ' + value)
     
 client = mqtt.Client()
